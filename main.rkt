df50750f67dcafffd8a84628e9906655c7784c03
jarcane <jarcane@users.noreply.github.com>
2014-08-08 23:10:26 +0300

    First commit.
    Just adding license files, still in design phase

---
+++ main.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+; MicroMini - A vintage minicomputer inspired stack machine
+;
+; Copyright 2014 John S. Berry III
+;
+;    This program is free software: you can redistribute it and/or modify
+;    it under the terms of the GNU General Public License as published by
+;    the Free Software Foundation, either version 3 of the License, or
+;    (at your option) any later version.
+;
+;    This program is distributed in the hope that it will be useful,
+;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;    GNU General Public License for more details.
+;
+;    You should have received a copy of the GNU General Public License
+;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
